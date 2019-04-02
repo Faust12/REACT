fc040efae85ec3dbf6954a014f1d4138b41ec723
Pavel <Pasha_sss@tut.by>
2019-04-02 12:03:53 +0300

    header without styles

---
+++ src/components/contacts.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Contacts extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>+375 (29) 123-45-67</p>
+                <p>mail@google.com</p>
+            </div>
+        );
+    }
+}
+
+export default Contacts;
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,18 +1,52 @@
 import React from 'react';
+import Contacts from './contacts.jsx'
 
 class Header extends React.Component {
-    render() {
-      return (
-        <div className="header">
-          <h1>Shopping List for {this.props.name}</h1>
-          <ul>
-            <li>Instagram</li>
-            <li>WhatsApp</li>
-            <li>Oculus</li>
-          </ul>
+  render() {
+    return (
+      <header>
+        <div>
+          <nav>
+            <div></div>
+            <ul>
+              <li><a href="index.html" class="selected">Главная</a></li>
+              <li><a href="delivery.html">Доставка и оплата</a></li>
+              <li><a href="about.html">О компании</a></li>
+              <li><a href="contacts.html">Контакты</a></li>
+              <li><a href="" onClick="getCatalog(event)">Каталог</a></li>
+            </ul>
+          </nav>
+          <div>
+            Корзина: 0 руб
+          </div>
         </div>
-      );
-    }
+        <div>
+          <a href="index.html"><img src="img/logo.jpg" alt="logo" /></a>
+          <Contacts />
+          <div>
+            <input type="text" placeholder="Поиск по каталогу" />
+            <button onClick="search(event)"></button>
+          </div>
+        </div>
+        <div>
+          <nav>
+            <ul>
+              <li><a href="#" onClick="showSigs(event, 'sig')">Электронные сигареты</a></li>
+              <li><a href="#" onClick="showSigs(event, 'fl')">Жидкости</a></li>
+              <li>
+                <a href="#">Комплектующие</a>
+                <ul>
+                  <li><a href="#">Дрипки</a></li>
+                  <li><a href="#">Баки</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Аккумуляторы</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    );
   }
-  
+}
+
 export default Header;
